7e68044f30c9da0d08114a63828ec1b01077678a
mariuspetrariu <mariuspetrariu@msn.com>
2019-03-29 19:35:44 +0200

    Initial commit

---
+++ core/src/inputhandlingpooling/sample/InputHandlingPooling.java
@@ -0,0 +1,63 @@
+package inputhandlingpooling.sample;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Logger;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class InputHandlingPooling extends ApplicationAdapter
+    {
+        private static final Logger             LOGGER_INFO = new Logger ( InputHandlingPooling.class.getName () , Logger.INFO );
+        private static       int                displayLogger;
+        private              SpriteBatch        batch;
+        private              BitmapFont         font;
+        private              OrthographicCamera camera;
+        private              Viewport           viewport;
+
+        @Override
+        public void create ()
+            {
+                camera = new OrthographicCamera ();
+                viewport = new FitViewport ( 1080 , 720 , camera );
+                batch = new SpriteBatch ();
+                font = new BitmapFont ( Gdx.files.internal ( "oswald-32.fnt" ) );
+            }
+
+        @Override
+        public void resize ( int width , int height )
+            {
+                viewport.update ( width , height , true );
+            }
+
+        @Override
+        public void render ()
+            {
+                GdxUtils.clearScreen ();
+                batch.setProjectionMatrix ( camera.combined );
+                batch.begin ();
+                int mouseX = Gdx.input.getX ();
+                int mouseY = Gdx.input.getY ();
+                if ( ( displayLogger % 60 ) == 0 )
+                    {
+                        LOGGER_INFO.info ( "mouse coordinates: mouse X = " + mouseX + " mouse Y = " + mouseY );
+                    }
+                displayLogger++;
+                font.draw ( batch , "mouse coordinates: x = " + mouseX + " y = " + mouseY , 20f , 720f - 20f );
+                boolean leftMouseButtonPressed  = Gdx.input.isButtonPressed ( Input.Buttons.LEFT );
+                boolean rightMouseButtonPressed = Gdx.input.isButtonPressed ( Input.Buttons.RIGHT );
+                font.draw ( batch , "left mouse button is" + ( leftMouseButtonPressed ? "" : "n't " ) + " pressed" , 20 , 720 - 60 );
+                font.draw ( batch , "right mouse button is" + ( rightMouseButtonPressed ? "" : "n't " ) + " pressed" , 20 , 720 - 100 );
+                batch.end ();
+            }
+
+        @Override
+        public void dispose ()
+            {
+                batch.dispose ();
+            }
+    }
---
+++ desktop/src/inputhandlingpooling/sample/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package inputhandlingpooling.sample.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import inputhandlingpooling.sample.InputHandlingPooling;
+
+public class DesktopLauncher
+    {
+        public static void main ( String[] arg )
+            {
+                LwjglApplicationConfiguration config = new LwjglApplicationConfiguration ();
+                new LwjglApplication ( new InputHandlingPooling () , config );
+            }
+    }
